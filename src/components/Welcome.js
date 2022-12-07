ca267ff9c50ba1c5f1124affdc06acff259878dd
CandeMartt <candemartt@gmail.com>
2022-12-07 00:33:57 -0300

    Clase 2 de React

---
+++ src/components/Welcome.js
@@ -0,0 +1,26 @@
+// Dependencia
+import React from "react";
+
+// Logica
+var gente = "Hola gente!"
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+}
+const element = (
+    <h1>
+        {formatName(user)}!
+    </h1>
+)
+
+// Render
+function Welcome(){
+    return <p>Hola, desde Welcome. {element} </p>
+}
+
+//Salida
+export default  Welcome
